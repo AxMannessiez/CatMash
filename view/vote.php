17eea9493d76e00eeb99a3f56bea12f886ae9a4a
Axel M <axelmannessiez@yahoo.fr>
2021-06-04 17:04:30 +0200

    Start with test images fo the first page (base html/css)

---
+++ view/vote.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>CatMash - Vote</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="view/style/common.css" />
+        <link rel="stylesheet" href="view/style/vote.css" />
+    </head>
+
+    <body>
+
+    	<header>
+    		<a href="index.php">
+    			<img src="view/img/CatMash.svg" alt="CatMash logo" id="CatMash_logo"/>
+    			<h1>Cat Mash</h1>
+    		</a>
+    	</header>
+
+    	<div id="main-div">
+    		
+			<div class="first cat">
+				<img src="http://24.media.tumblr.com/tumblr_m82woaL5AD1rro1o5o1_1280.jpg" width="100px"/>
+			</div>
+
+			<div class="second cat">
+				<img src="http://25.media.tumblr.com/tumblr_m4pwa9EXE41r6jd7fo1_500.jpg" width="100px"/>
+			</div>
+
+    	</div>
+
+
+
+    </body>
+
+
+</html>
