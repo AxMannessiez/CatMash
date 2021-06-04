ed840b6d65bd3448679c6b09313ac62d8a1db677
Axel M <axelmannessiez@yahoo.fr>
2021-06-04 15:49:00 +0200

    First requests for the Cats table

--- model/cats_requests.php
+++ model/cats_requests.php
@@ -1,6 +1,38 @@
 <?php
 
-function AddCat(PDO $db, string $id, string $url) {
+// Queries concerning the Cats table
+
+/**
+ * Add a Cat to the table
+ * @param PDO $db
+ * @param string $id
+ * @param string $url
+ * @return array
+ */
+function AddCat(PDO $db, string $id, string $url): array {
 	$add = $db->prepare("INSERT INTO Cats (name, url, ranking, votes) VALUES (?, ?, 1200, 0)");
 	$add->execute(array($id, $url));
+}
+
+/**
+ * Get the total cats number (the size of the Cats table)
+ * @param PDO $db
+ * @return int
+ */
+function GetCatNumber(PDO $db): int {
+	$query = $db->prepare("SELECT COUNT(id) as size FROM Cats");
+	$query->execute();
+	return $query->fetch()["size"];
+}
+
+/**
+ * Get the cat corresponding to a given id
+ * @param PDO $db
+ * @param int $id
+ * @return array
+ */
+function GetCatFromId(PDO $db, int $id): array {
+	$query = $db->prepare("SELECT * FROM Cats WHERE id = ?");
+	$query->execute(array($id));
+	return $query->fetch();
 }
